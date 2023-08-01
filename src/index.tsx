df764ad400b65df8b0aaed9adf0d6acd174aa62d
DongHyeonWon <dhe7700@naver.com>
2023-08-01 19:52:09 +0900

    feat: css reset 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Global } from "@emotion/react";
+import reset from "./styles/reset";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Global styles={reset} />
     <App />
   </React.StrictMode>
 );
