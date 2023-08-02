fadf2ac5dfebf48dad67109ac350f8cac6fc3770
DongHyeonWon <dhe7700@naver.com>
2023-08-03 01:25:44 +0900

    feat: ModalContainer 추가

---
+++ src/FilterModal/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ModalContainer from "../components/Container/ModalContainer";
+
+export default function FilterModal() {
+  return (
+    <ModalContainer clicked={true}>
+      <div>필터</div>
+    </ModalContainer>
+  );
+}
--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Wrapper from "../Container/Wrapper";
+import FilterModal from "../../FilterModal";
 
 export default function Search() {
-  return <Wrapper>Search</Wrapper>;
+  return (
+    <Wrapper>
+      <FilterModal />
+    </Wrapper>
+  );
 }
