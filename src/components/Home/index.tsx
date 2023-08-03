e71145a04d195169587fd80b70128086ba322d26
daeunleeeee <lde7953@gmail.com>
2023-08-03 15:56:56 +0900

    feat: RowCard(1)

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,8 +1,27 @@
+/** @jsxImportSource @emotion/react */
+
 import { useNavigate } from "react-router-dom";
 import Button from "../button/Button";
 import ColumnCard from "../card/ColumnCard";
-import RowCard from "../card/RowCard" //RowCard 추가
+import RowCard from "../card/RowCard";
+import { css } from "@emotion/react";
 import Wrapper from "../Container/Wrapper";
+
+const rankText = css`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+const cards = css`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 0.1rem;
+  border-radius: 1rem;
+  border: 1px solid black;
+  height: 30rem;
+  margin: auto;
+`;
 
 export default function Home() {
   const navigate = useNavigate();
@@ -12,6 +31,38 @@ export default function Home() {
       <Wrapper>
         <div onClick={() => navigate(`/search`)}>누르면 search 로 가용</div>
         <Button />
+        <div css={rankText}>실시간 랭킹</div>
+        <div css={cards}>
+          <RowCard
+            img="https://img2.joongna.com/media/original/2023/08/02/1690959165531TTO_wYyoM.jpg?impolicy=resizeWatermark3&ftext=%EC%8B%9C%ED%95%B4"
+            title="아이폰 12 미니"
+            highestPrice="300,000"
+            lowestPrice="180,000"
+            url="https://web.joongna.com/product/125014218"
+          />
+          <RowCard
+            img="https://img2.joongna.com/media/original/2023/08/02/1690959165531TTO_wYyoM.jpg?impolicy=resizeWatermark3&ftext=%EC%8B%9C%ED%95%B4"
+            title="아이폰 12 미니"
+            highestPrice="300,000"
+            lowestPrice="180,000"
+            url="https://web.joongna.com/product/125014218"
+          />
+          <RowCard
+            img="https://img2.joongna.com/media/original/2023/08/02/1690959165531TTO_wYyoM.jpg?impolicy=resizeWatermark3&ftext=%EC%8B%9C%ED%95%B4"
+            title="아이폰 12 미니"
+            highestPrice="300,000"
+            lowestPrice="180,000"
+            url="https://web.joongna.com/product/125014218"
+          />
+          <RowCard
+            img="https://img2.joongna.com/media/original/2023/08/02/1690959165531TTO_wYyoM.jpg?impolicy=resizeWatermark3&ftext=%EC%8B%9C%ED%95%B4"
+            title="아이폰 12 미니"
+            highestPrice="300,000"
+            lowestPrice="180,000"
+            url="https://web.joongna.com/product/125014218"
+          />
+        </div>
+
         <ColumnCard
           title="테스트입니다"
           price="200,000"
