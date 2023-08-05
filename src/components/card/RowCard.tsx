a636982159085ff79d5e712ae33ecb16787e28b6
KimJJRoSY <kjjeaus@gmail.com>
2023-08-05 20:47:16 +0900

    feat:ArrangeButton

--- src/components/card/RowCard.tsx
+++ src/components/card/RowCard.tsx
@@ -22,32 +22,32 @@ const background = css`
   font-weight: bold;
 `;
 
-const space = css `
+const space = css`
   width: 0.5rem;
 `;
 
-const line_space = css `
+const line_space = css`
   margin-top: 0.2rem;
 `;
 
 const outerDiv = css`
-    margin-top: 1rem;
-    width: 18rem;
-    height: 20rem;
-    border: 0.13rem solid;
-    border-radius: 0.8rem;
-    border-color: ${theme.colors["main-color"]};
+  margin-top: 1rem;
+  width: 18rem;
+  height: 20rem;
+  border: 0.13rem solid;
+  border-radius: 0.8rem;
+  border-color: ${theme.colors["main-color"]};
 `;
 
-const innerDiv = css `
-    display: flex;
-    justify-content : center;
-    width: 16rem;
-    margin-top: 1rem;
-    margin-left: 0.9rem;
-    border: 0.13rem solid;
-    border-color: ${theme.colors["main-color"]};
-    border-radius: 2rem;
+const innerDiv = css`
+  display: flex;
+  justify-content: center;
+  width: 16rem;
+  margin-top: 1rem;
+  margin-left: 0.9rem;
+  border: 0.13rem solid;
+  border-color: ${theme.colors["main-color"]};
+  border-radius: 2rem;
 `;
 
 //상품 이미지
@@ -58,14 +58,14 @@ const productImg = css`
 `;
 
 //플랫폼 이름, 상품 제목, 가격, 위치
-const content = css `
-    margin-left: 2rem;
-    margin-top: 0.5rem;
+const content = css`
+  margin-left: 2rem;
+  margin-top: 0.5rem;
 `;
 
-const popularProduct = css `
-font-size: 1.8rem;
-font-weight: bold;
+const popularProduct = css`
+  font-size: 1.8rem;
+  font-weight: bold;
 `;
 
 export default function RowCard({
@@ -78,23 +78,23 @@ export default function RowCard({
 }: RowCardProps) {
   return (
     <>
-    <div css={popularProduct}>인기 매물</div>
-    <div css={background}>
-      <div onClick={() => window.open(url)}>
-        <div css={outerDiv}> 
-         <div css={innerDiv}>
-            <img css={productImg} src={img} alt="img" />
-         </div>
-         <div css={content}>
-            <div>{shop}</div>
-            <div css={line_space}>{title}</div>
-            <div css={line_space}>{price}원</div>
-            <div css={line_space}>{location}</div>
+      <div css={popularProduct}>인기 매물</div>
+      <div css={background}>
+        <div onClick={() => window.open(url)}>
+          <div css={outerDiv}>
+            <div css={innerDiv}>
+              <img css={productImg} src={img} alt="img" />
+            </div>
+            <div css={content}>
+              <div>{shop}</div>
+              <div css={line_space}>{title}</div>
+              <div css={line_space}>{price}원</div>
+              <div css={line_space}>{location}</div>
+            </div>
+          </div>
         </div>
-        </div>
+        <div css={space}></div>
       </div>
-      <div css={space}></div>       
-    </div> 
     </>
   );
 }
