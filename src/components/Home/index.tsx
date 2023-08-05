7034486a20c389ce71165a2a3ee63b93b7b9471d
Kang soyeong <korea020808@naver.com>
2023-08-05 20:37:46 +0900

    feat: 메인 화면 검색창 추가

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,8 @@
-/** @jsxImportSource @emotion/react */
-
 import { useNavigate } from "react-router-dom";
-import RankCard from "../card/RankCard";
-import { css } from "@emotion/react";
+// import Button from "../button/Button";
+import RowCard from "../card/RowCard" //RowCard 추가
 import Wrapper from "../Container/Wrapper";
+<<<<<<< HEAD
 import RowCard from "../card/RowCard";
 import Header from "../Header/Header";
 import Search from "../searchBar/Search"
@@ -23,19 +22,27 @@ const cards = css`
   height: 30rem;
   margin: auto;
 `;
+=======
+import Search from "../searchBar/Search"
+>>>>>>> 04306fa (feat: 메인 화면 검색창 추가)
 
 export default function Home() {
   const navigate = useNavigate();
 
   const handleSearch = (searchTerm: string) => {
     console.log("검색:", searchTerm);
+<<<<<<< HEAD
 
   };
+=======
+  };
+
+>>>>>>> 04306fa (feat: 메인 화면 검색창 추가)
   return (
     <>
-      <Header />
       <Wrapper>
         <Search onSearch={handleSearch} />
+<<<<<<< HEAD
         <div css={rankText}>실시간 랭킹</div>
         <div css={cards}>
           <RankCard
@@ -46,16 +53,31 @@ export default function Home() {
             url="https://web.joongna.com/product/125014218"
           />
         </div>
+=======
+        {/* <div onClick={() => navigate(`/search`)}>누르면 search 로 가용</div>
+        <Button />
+        <ColumnCard
+          title="테스트입니다"
+          price="200,000"
+          url="https://www.naver.com"
+          location="서울시 강동구"
+          img="https://image.lguplus.com/common/images/hphn/product/A2628-128/imge_cut/ushop_A2628-128_67_A.jpg"
+        /> */}
+
+        <div>인기 매물</div> 
+>>>>>>> 04306fa (feat: 메인 화면 검색창 추가)
         <div>
-          <RowCard
-            title="아이폰 11 퍼플"
-            price="500,000"
-            shop="당근마켓"
-            url=""
-            location="인천광역시 부평구"
-            img="https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone_11-rosette-family-lineup-091019_big.jpg.large.jpg"
-          />
+          <RowCard 
+          title = "아이폰 11 퍼플"
+          price="500,000"
+          shop = "당근마켓"
+          url = ""
+          location = "인천광역시 부평구"
+          img = "https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone_11-rosette-family-lineup-091019_big.jpg.large.jpg"
+        />
+        
         </div>
+        
       </Wrapper>
     </>
   );
