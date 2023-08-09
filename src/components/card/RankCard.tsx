bf5b532893e3581c4a4c181c16ddbdbc6be41d4a
daeunleeeee <lde7953@gmail.com>
2023-08-09 21:31:57 +0900

    feat: enlarged Rank Card

--- src/components/card/RankCard.tsx
+++ src/components/card/RankCard.tsx
@@ -8,37 +8,13 @@ interface RankCardProps {
   title: string;
   highestPrice: string;
   lowestPrice: string;
+  css?: {
+    rankTest?: any;
+    imgEx?: any;
+    texts?: any;
+    product?: any;
+  };
 }
-
-const rankTest = css`
-  width: 33rem;
-  height: 13rem;
-  border-radius: 1rem;
-  border: 1px solid black;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  margin: auto;
-  justify-content: center;
-  align-items: center;
-`;
-
-const imgEx = css`
-  width: 15rem;
-  height: 11rem;
-  border-radius: 1rem;
-  margin-left: 1rem;
-`;
-
-const texts = css`
-  font-size: 1rem;
-  line-height: 200%;
-  margin-left: 1rem;
-`;
-
-const product = css`
-  font-size: 1.2rem;
-  font-weight: bold;
-`;
 
 export default function RankCard({
   url,
@@ -46,10 +22,50 @@ export default function RankCard({
   title,
   highestPrice,
   lowestPrice,
+  css: customStyles,
 }: RankCardProps) {
+  const rankTest = css`
+    width: 33rem;
+    height: 13rem;
+    border-radius: 1rem;
+    border: 1px solid black;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    margin: auto;
+    justify-content: center;
+    align-items: center;
+    ${customStyles?.rankTest}
+  `;
+
+  const imgEx = css`
+    width: 15rem;
+    height: 11rem;
+    border-radius: 1rem;
+    margin-left: 1rem;
+    ${customStyles?.imgEx}
+  `;
+
+  const texts = css`
+    font-size: 1rem;
+    line-height: 200%;
+    margin-left: 1rem;
+    ${customStyles?.texts}
+  `;
+
+  const product = css`
+    font-size: 1.2rem;
+    font-weight: bold;
+    ${customStyles?.product}
+  `;
+
+  const handleClick = () => {
+    if (url) {
+      window.open(url);
+    }
+  };
   return (
     <>
-      <div onClick={() => window.open(url)} css={rankTest}>
+      <div onClick={handleClick} css={rankTest}>
         <img css={imgEx} src={img} alt="img" />
         <div css={texts}>
           <div css={product}>{title}</div>
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -4,15 +4,52 @@ import SearchResultBar from "../components/SearchResultsBar/SearchResultsBar";
 import FilterModal from "../components/modal/FilterModal";
 import { useSetRecoilState } from "recoil";
 import { filterClicked } from "../recoil/atoms/filterClicked";
+import rank from "../data/rank.json";
+import RankCard from "../components/card/RankCard";
+import { css } from "@emotion/react";
 
 export default function Search() {
   const setClicked = useSetRecoilState(filterClicked);
+
   return (
     <Wrapper>
       <div onClick={() => setClicked((prev) => !prev)}>
         누르면 필터가 나옵니다
       </div>
       <SearchResultBar result="234335" />
+      {rank.map(({ image, model, modelHp, modelLp, id }) => {
+        return (
+          <RankCard
+            key={id}
+            img={image}
+            title={model}
+            highestPrice={modelHp}
+            lowestPrice={modelLp}
+            url=""
+            css={{
+              rankTest: css`
+                width: 65rem;
+                height: 30rem;
+              `,
+              imgEx: css`
+                width: 30rem;
+                height: 20rem;
+                margin-left: 5rem;
+                border: solid black;
+                border-radius: none;
+              `,
+              texts: css`
+                font-size: 1.5rem;
+                line-height: 250%;
+                margin-left: 3rem;
+              `,
+              product: css`
+                font-size: 2.5rem;
+              `,
+            }}
+          />
+        );
+      })}
       <FilterModal title="필터" />
     </Wrapper>
   );
