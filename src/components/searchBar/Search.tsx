cc2964a8aacd8bc6bb22cb50474acb416fcdd51f
DongHyeonWon <dhe7700@naver.com>
2023-08-10 01:07:38 +0900

    fix: Search 컴포넌트 수정

--- src/components/searchBar/Search.tsx
+++ src/components/searchBar/Search.tsx
@@ -36,7 +36,7 @@ const searchInput = css`
 const searchButton = css`
   right: 1rem;
   padding: 0.5rem 1rem;
-  background-color: ${theme.colors["light-gray"]}
+  background-color: ${theme.colors["light-gray"]};
   border: none;
   width: 4.2rem;
   height: 2.5rem;
@@ -64,71 +64,72 @@ const history = css`
 //여기까지 CSS
 
 const Search: React.FC<SearchProps> = ({ onSearch }) => {
-    // 검색어 상태&검색 기록 상태 초기화
-    const [searchTerm, setSearchTerm] = useState("");
-    const [searchHistory, setSearchHistory] = useState<string[]>([]);
-    const [isHistoryVisible, setIsHistoryVisible] = useState(false); // 검색 기록 창 보이기 여부
-    const historyRef = useRef<HTMLDivElement>(null);
-  
-    // 검색 기록이 추가될 때마다 높이 조정
-    useEffect(() => {
-      if (historyRef.current) {
-        historyRef.current.style.height = "auto"; // 높이 초기화
-        historyRef.current.style.height = `${historyRef.current.scrollHeight}px`;
-      }
-    }, [searchHistory]);
-  
-    // 검색어 입력 이벤트 핸들러
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setSearchTerm(event.target.value);
-    };
-  
-    // 검색 버튼 클릭 이벤트 핸들러
-    const handleSearch = () => {
-      if (searchTerm.trim() !== "") {
-        // 검색어가 비어있지 않은 경우 검색 동작 처리
-        // 검색 동작을 위해 onSearch 함수 호출하고 검색어를 인자로 전달
-        onSearch(searchTerm);
-  
-        // 검색 기록 상태를 업데이트하여 새로운 검색어를 기록에 추가
-        setSearchHistory((prevHistory) => [...prevHistory, searchTerm]);
-  
-        // 검색어 상태를 초기화해 입력 창 비우기
-        setSearchTerm("");
-      }
-    };
-  
-    // 엔터키 누를 때 검색 동작을 실행하기 위한 이벤트 핸들러
-    const handleKeyPress = (
-      event: React.KeyboardEvent<HTMLInputElement>
-    ): void => {
-      // 엔터키가 눌렸을 때 handleSearch 함수를 호출한 다음 검색 실행
-      if (event.key === "Enter") {
-        handleSearch();
-      }
-    };
-    const handleSearchBarClick = () => {
-      setIsHistoryVisible(!isHistoryVisible);
-    };
-  
-    // Search 컴포넌트 렌더링 결과 반환
-    return (
-      <div>
-        <div onClick={handleSearchBarClick} css={searchWrapper}>
-          <input //검색어 입력
-            css={searchInput}
-            type="text"
-            value={searchTerm}
-            onChange={handleInputChange}
-            onKeyPress={handleKeyPress} // 엔터키 누를 때 검색되도록 함
-            placeholder="검색어를 입력하세요"
-          />
-          {/* 검색버튼 */}
-          <button onClick={handleSearch} css={searchButton}>
-            검색
-          </button>
-        </div>
-        {searchHistory.length > 0 && isHistoryVisible && ( //검색 기록이 있는 경우에만 검색 기록 창 보여주도록 함
+  // 검색어 상태&검색 기록 상태 초기화
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchHistory, setSearchHistory] = useState<string[]>([]);
+  const [isHistoryVisible, setIsHistoryVisible] = useState(false); // 검색 기록 창 보이기 여부
+  const historyRef = useRef<HTMLDivElement>(null);
+
+  // 검색 기록이 추가될 때마다 높이 조정
+  useEffect(() => {
+    if (historyRef.current) {
+      historyRef.current.style.height = "auto"; // 높이 초기화
+      historyRef.current.style.height = `${historyRef.current.scrollHeight}px`;
+    }
+  }, [searchHistory]);
+
+  // 검색어 입력 이벤트 핸들러
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // 검색 버튼 클릭 이벤트 핸들러
+  const handleSearch = () => {
+    if (searchTerm.trim() !== "") {
+      // 검색어가 비어있지 않은 경우 검색 동작 처리
+      // 검색 동작을 위해 onSearch 함수 호출하고 검색어를 인자로 전달
+      onSearch(searchTerm);
+
+      // 검색 기록 상태를 업데이트하여 새로운 검색어를 기록에 추가
+      setSearchHistory((prevHistory) => [...prevHistory, searchTerm]);
+
+      // 검색어 상태를 초기화해 입력 창 비우기
+      setSearchTerm("");
+    }
+  };
+
+  // 엔터키 누를 때 검색 동작을 실행하기 위한 이벤트 핸들러
+  const handleKeyPress = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ): void => {
+    // 엔터키가 눌렸을 때 handleSearch 함수를 호출한 다음 검색 실행
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+  const handleSearchBarClick = () => {
+    setIsHistoryVisible(!isHistoryVisible);
+  };
+
+  // Search 컴포넌트 렌더링 결과 반환
+  return (
+    <div>
+      <div onClick={handleSearchBarClick} css={searchWrapper}>
+        <input //검색어 입력
+          css={searchInput}
+          type="text"
+          value={searchTerm}
+          onChange={handleInputChange}
+          onKeyPress={handleKeyPress} // 엔터키 누를 때 검색되도록 함
+          placeholder="검색어를 입력하세요"
+        />
+        {/* 검색버튼 */}
+        <button onClick={handleSearch} css={searchButton}>
+          검색
+        </button>
+      </div>
+      {searchHistory.length > 0 &&
+        isHistoryVisible && ( //검색 기록이 있는 경우에만 검색 기록 창 보여주도록 함
           <div ref={historyRef} css={history}>
             {/* 검색 기록 리스트 */}
             <ul>
@@ -138,8 +139,8 @@ const Search: React.FC<SearchProps> = ({ onSearch }) => {
             </ul>
           </div>
         )}
-      </div>
-    );
-  };
-  
-  export default Search;
+    </div>
+  );
+};
+
+export default Search;
