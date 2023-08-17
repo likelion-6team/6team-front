67293698a0034b035db88e5e0008b5c3ca6f8666
Kang soyeong <korea020808@naver.com>
2023-08-17 21:22:14 +0900

    feat: 날짜 변환 함수 삭제

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -23,12 +23,6 @@ export default function Search() {
   if (isError) {
     return <p>Error loading data.</p>;
   }
-  
-  // 날짜 데이터 변환
-  const transformedData = data.data.map((d: any) => ({
-    ...d,
-    formattedDate: formatDate(d.date),
-  }));
 
   return (
     <Wrapper>
@@ -60,32 +54,23 @@ export default function Search() {
         />)
         )}}   */}
 
+      <SearchResultBar result="234335"/>
       <GridContainer>
-        {transformedData.map((d: any, index: number) => (
-          <ColumnCard
-            key={index}
-            title={d.title}
-            price={d.price}
-            shop={d.site}
-            url={d.url}
-            location={d.region}
-            img={d.image}
-            date={d.formattedDate} //여기를 d.date로 수정해야할지도
-          />
-        ))}
+        {data.data &&
+          data.data.map((d: any, index: number) => (
+            <ColumnCard
+              key={index}
+              title={d.title}
+              price={d.price}
+              shop={d.site}
+              url={d.url}
+              location={d.region}
+              img={d.image}
+              date={d.date}
+            />
+          ))}
       </GridContainer>
       <FilterModal title="필터" />
     </Wrapper>
   );
-}
-
-// 날짜 포맷 변환 함수
-// API로부터 받아오는 날짜는 20230810의 형태인데
-// toLocaleDateString를 사용하려면 2023-08-10의 형태가 되어야 합니다.
-// 따라서 날짜 포맷 변환 함수를 따로 작성해 주었습니다.
-function formatDate(rawDate: string) {
-  const year = rawDate.substr(0, 4); // 문자열의 첫 번째부터 4개의 문자열 추출 - 2023
-  const month = rawDate.substr(4, 2); // 문자열의 다섯 번째부터 2개의 문자열 추출 - 08
-  const day = rawDate.substr(6, 2); // 문자열의 일곱 번째부터 2개의 문자열 추출 - 10
-  return `${year}-${month}-${day}`; // 2023-08-10 리턴
 }
